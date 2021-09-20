e04aca865471f3d6a8852b2032c7c72e3e8ff729
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-09-20 13:02:36 +0530

    Add files via upload

---
+++ HELLOW.C
@@ -0,0 +1,24 @@
+//(Single Line Comment)
+/*
+	Multiple Line
+	Comment
+*/
+
+// Pre-Processor Directive
+#include<stdio.h>
+#include<conio.h> // it is optional to mention
+
+/*
+	# - Pre-Processor
+	include
+	Directive: Directory / Folder
+	<stdio.h>: Standard Input / Output
+	<conio.h>: Console Input / Output
+*/
+// main() - Entry point Function
+// return
+void main() { // void - null or empty
+	clrscr(); // clear screen
+	printf("Hello World"); // : - colon, ; - semi-colon: terminate the line
+	getch(); // get character
+}
