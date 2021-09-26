c15baedcea194e6171d605c480875f9423489f3b
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-09-26 13:05:01 +0530

    Add files via upload

---
+++ DATATYPE.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+//	unsigned short int x = 65536;
+//	short int x = 32768;
+	short int x = -4;
+	clrscr();
+
+	printf("X is: %d\n", x);
+	printf("X is: %u",x); // u - unsigned
+
+	getch();
+}
